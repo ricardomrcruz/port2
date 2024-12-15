b868b9f0c377f79e0149148cf7a6ee5dfaffe914
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-12-15 20:28:53 +0100

    .

--- portfolio2.0/src/components/accordion1.tsx
+++ portfolio2.0/src/components/accordion1.tsx
@@ -121,27 +121,27 @@ export default function Accordion() {
           </ul>
         </p>
       </div>
-      {/* WORK NUMBER 2 */}
-      <div id="accordion-open-heading-2">
+      {/* WORK NUMBER 4 */}
+      <div id="accordion-open-heading-4 ">
         <button
           type="button"
-          className="group flex items-center justify-between w-full  font-medium rtl:text-right gap-3"
-          aria-expanded={openAccordion["accordion-open-body-2"]}
-          aria-controls="accordion-open-body-2"
-          onClick={() => toggleAccordion("accordion-open-body-2")}
+          className="group flex items-center justify-between w-full  font-medium rtl:text-right  gap-3"
+          aria-expanded={openAccordion["accordion-open-body-4"]}
+          aria-controls="accordion-open-body-4"
+          onClick={() => toggleAccordion("accordion-open-body-4")}
         >
           <span className="flex items-center">
             <span className=" flex justify-center items-center overflow-hidden ">
               <img
-                src="/logo3.jpg"
+                src="/logo7.jpg"
                 alt="cbdeau"
                 className="h-10 w-10 sm:h-[3.5rem] sm:w-[3.5rem] rounded-full object-cover "
               />
             </span>
             <div className="text-left px-4">
-              <p className="text-base sm:text-lg">Strasbourg Web Solutions</p>
+              <p className="text-base sm:text-lg">Green Distribution France</p>
               <p className="text-[11px] sm:text-xs text-gray-200">
-                Fullstack Web Developer Freelancer
+                Web Developer
               </p>
             </div>
             <div className="text-left ">
@@ -150,7 +150,7 @@ export default function Accordion() {
                   data-accordion-icon
                   className={`w-3 h-3  shrink-0 transition-all duration-300 ease-out opacity-0 group-hover:opacity-100 
                 ${
-                  openAccordion["accordion-open-body-2"]
+                  openAccordion["accordion-open-body-4"]
                     ? "rotate-180 "
                     : "rotate-90 "
                 }`}
@@ -172,17 +172,103 @@ export default function Accordion() {
             </div>
           </span>
           <div>
+            <span className="text-[11px] sm:text-sm text-gray-500 ">
+              August 2023 - October 2023
+            </span>
+            <p aria-hidden="true" className="h-7" />
+          </div>
+        </button>
+      </div>
+
+      <div
+        id="accordion-open-body-1 "
+        aria-labelledby="accordion-open-heading-4"
+        className={` transition-all ease-out overflow-hidden py-3 px-2 text-sm ${
+          openAccordion["accordion-open-body-4"]
+            ? "max-h-96 opacity-100 "
+            : "max-h-0 opacity-0 "
+        }`}
+      >
+        <p className=" text-gray-300 ">
+          <ul className="space-y-4">
+            <li>
+              Led the development and maintenance of a suite of B2B e-commerce
+              platforms for ARAN HOLDING subsidiary, managing multiple branded
+              sites including green-distrib.fr, cbdburaliste.fr,
+              bigcbdproduct.com, cbdeau.be, and californiakush.fr.
+            </li>
+
+            <li>
+              Delivered comprehensive full-stack CMS solutions using PHP/MySQL
+              and HTML/CSS/JavaScript, while driving UX/UI design improvements.
+              Engineered and implemented an automated billing system,
+              streamlining B2B client network management and improving
+              operational efficiency.
+            </li>
+          </ul>
+        </p>
+      </div>
+      {/* WORK NUMBER 2 */}
+      <div id="accordion-open-heading-3">
+        <button
+          type="button"
+          className="group flex items-center justify-between w-full  font-medium rtl:text-right gap-3"
+          aria-expanded={openAccordion["accordion-open-body-2"]}
+          aria-controls="accordion-open-body-3"
+          onClick={() => toggleAccordion("accordion-open-body-2")}
+        >
+          <span className="flex items-center">
+            <span className=" flex justify-center items-center overflow-hidden">
+              <img
+                src="/logo2.jpg"
+                alt="cbdeau"
+                className="h-10 w-10 sm:h-[3.5rem] sm:w-[3.5rem] rounded-full object-cover "
+              />
+            </span>
+            <div className="text-left px-4">
+              <p className="text-base sm:text-lg">MyMetal® Dangolsheim</p>
+              <p className="text-[11px] sm:text-xs text-gray-200">
+                Web Developer
+              </p>
+            </div>
+            <div className="text-left ">
+              <p>
+                <svg
+                  data-accordion-icon
+                  className={`w-3 h-3  shrink-0 transition-all duration-300 ease-out opacity-0 group-hover:opacity-100${
+                    openAccordion["accordion-open-body-2"]
+                      ? "rotate-180 "
+                      : "rotate-90 "
+                  }`}
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 10 6"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M9 5 5 1 1 5"
+                  />
+                </svg>
+              </p>
+              <p aria-hidden="true" className="h-6" />
+            </div>
+          </span>
+          <div>
             <span className="text-[11px] sm:text-sm text-gray-500 justify-end">
-              June 2021 - Today
+              December 2023 - Mars 2023
             </span>
             <p aria-hidden="true" className="h-7" />
           </div>
         </button>
       </div>
       <div
-        id="accordion-open-body-2 "
+        id="accordion-open-body-2"
         aria-labelledby="accordion-open-heading-2"
-        className={` transition-all ease-out overflow-hidden py-3 px-2 text-sm  ${
+        className={` transition-all ease-out overflow-hidden  py-3 px-2 text-sm ${
           openAccordion["accordion-open-body-2"]
             ? "max-h-96 opacity-100 "
             : "max-h-0 opacity-0 "
@@ -191,21 +277,14 @@ export default function Accordion() {
         <p className=" text-gray-300">
           <ul className="space-y-4">
             <li>
-              Self-employed: IT services and consulting - freelance status.
+              Design and deployment of a company showcase website, ranked Top 3
+              on Google Search for targeted keywords.
             </li>
 
             <li>
-              Delivered custom e-commerce solutions and showcase websites
-              (WordPress, PrestaShop, Custom Applications). Provided technical
-              consulting and expertise to clients in their digital
-              transformation.
-            </li>
-
-            <li>
-              Co-founder of Mark3ts.io - e-commerce web-crawling SaaS service
-              (Python, FastAPI) enabling large-scale automated product data
-              extraction and monitoring for retailers (Amazon, eBay, NVIDIA,
-              Apple) and real-time integration - in development.
+              Full project ownership: UX/UI design, Figma wireframing,
+              React/JavaScript HTML/TailwindCSS frontend development, and Google
+              Business optimization.
             </li>
           </ul>
         </p>
@@ -222,15 +301,15 @@ export default function Accordion() {
           <span className="flex items-center">
             <span className=" flex justify-center items-center overflow-hidden ">
               <img
-                src="/logo2.jpg"
+                src="/logo3.jpg"
                 alt="cbdeau"
                 className="h-10 w-10 sm:h-[3.5rem] sm:w-[3.5rem] rounded-full object-cover "
               />
             </span>
             <div className="text-left px-4">
-              <p className="text-base sm:text-lg">MyMetal®</p>
+              <p className="text-base sm:text-lg">Strasbourg Web Solutions</p>
               <p className="text-[11px] sm:text-xs text-gray-200">
-                Intern Web Developer
+                Fullstack Web Developer Freelancer
               </p>
             </div>
             <div className="text-left ">
@@ -262,16 +341,16 @@ export default function Accordion() {
           </span>
           <div>
             <span className="text-[11px] sm:text-sm text-gray-500 justify-end">
-              January 2023 - Mars 2023
+              June 2021 - Today
             </span>
             <p aria-hidden="true" className="h-7" />
           </div>
         </button>
       </div>
       <div
-        id="accordion-open-body-3"
+        id="accordion-open-body-3 "
         aria-labelledby="accordion-open-heading-3"
-        className={` transition-all ease-out overflow-hidden  py-3 px-2 text-sm ${
+        className={` transition-all ease-out overflow-hidden py-3 px-2 text-sm  ${
           openAccordion["accordion-open-body-3"]
             ? "max-h-96 opacity-100 "
             : "max-h-0 opacity-0 "
@@ -280,14 +359,21 @@ export default function Accordion() {
         <p className=" text-gray-300">
           <ul className="space-y-4">
             <li>
-              Design and deployment of a company showcase website, ranked Top 3
-              on Google Search for targeted keywords.
+              Self-employed: IT services and consulting - freelance status.
             </li>
 
             <li>
-              Full project ownership: UX/UI design, Figma wireframing,
-              React/JavaScript frontend development, and Google Business
-              optimization.
+              Delivered custom e-commerce solutions and showcase websites
+              (WordPress, PrestaShop, Custom Applications). Provided technical
+              consulting and expertise to clients in their digital
+              transformation.
+            </li>
+
+            <li>
+              Co-founder of Mark3ts.io - e-commerce web-crawling SaaS service
+              (Python, FastAPI) enabling large-scale automated product data
+              extraction and monitoring for retailers (Amazon, eBay, NVIDIA,
+              Apple) and real-time integration - in development.
             </li>
           </ul>
         </p>
